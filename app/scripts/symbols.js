f6f99f7e89fc1cfe14bc026700940bc839363ac5
Anders Steiner <anders.steiner@gmail.com>
2014-12-16 09:54:09 +0100

    Separated symbols from main.js file to symbols.js

---
+++ app/scripts/symbols.js
@@ -0,0 +1,42 @@
+symbols = [
+        {id: '^GSPC', text: 'S&P-500', links: [
+                {text: 'Avanza', link: 'https://www.avanza.se/index/om-indexet.html/19004/s-p-500'}
+            ]},
+        {id: '^OMX', text: 'OMXS30', links: [
+                {text: 'Avanza', link: 'https://www.avanza.se/index/om-indexet.html/19002/omx-stockholm-30'}
+            ]},
+        {id: 'PXX.TO', text: 'Black Pearl Resources TMX', links: [
+                {text: 'Avanza OMX', link: 'https://www.avanza.se/aktier/om-aktien.html/85707/blackpearl-resources-sdb'},
+                {text: 'Avanza TMX', link: 'https://www.avanza.se/aktier/om-aktien.html/140688/blackpearl-resources-inc'},
+                {text: 'Avanza Forum', link: 'https://www.avanza.se/placera/forum/forum/blackpearl-resources-sdb.html'},
+                {text: 'Canadian Insider', link: 'https://canadianinsider.com/node/7?menu_tickersearch=pxx'},
+                {text: 'Stockwatch Shorts', link: 'http://www.stockwatch.com/Analytics/Shorts.aspx?action=go&symbol=PXX&region=C'},
+                {text: 'Stockhouse', link: 'http://www.stockhouse.com/companies/bullboard/t.pxx/blackpearl-resources-inc'},
+                {text: 'Big Charts OMX', link: 'http://bigcharts.marketwatch.com/kaavio.Webhost/charts/big.chart?type=64&size=4&state=9&style=380&freq=5mi&time=1day&symb=se:pxxssdb'},
+                {text: 'Big Charts TMX', link: 'http://bigcharts.marketwatch.com/kaavio.Webhost/charts/big.chart?type=64&size=4&state=9&style=380&freq=5mi&time=1day&symb=ca:pxx'}
+
+            ]},
+        {id: 'AOI.ST', text: 'Africa Oil OMX', links: [
+                {text: 'Avanza OMX', link: 'https://www.avanza.se/aktier/om-aktien.html/250993/africa-oil'},
+                {text: 'Avanza TMX', link: 'https://www.avanza.se/aktier/om-aktien.html/476936/africa-oil'},
+                {text: 'Avanza Forum', link: 'https://www.avanza.se/placera/forum/forum/africa-oil-corp.html'},
+                {text: 'Canadian Insider', link: 'https://canadianinsider.com/node/7?menu_tickersearch=aoi'},
+                {text: 'Stockwatch Shorts', link: 'http://www.stockwatch.com/Analytics/Shorts.aspx?action=go&symbol=AOI&region=C'},
+                {text: 'Stockhouse', link: 'http://www.stockhouse.com/companies/bullboard/v.aoi/africa-oil-corp'},
+                {text: 'Big Charts Sweden', link: 'http://bigcharts.marketwatch.com/kaavio.Webhost/charts/big.chart?type=64&size=4&state=9&style=380&freq=5mi&time=1day&symb=se:aoi'},
+                {text: 'Big Charts Canada', link: 'http://bigcharts.marketwatch.com/kaavio.Webhost/charts/big.chart?type=64&size=4&state=9&style=380&freq=5mi&time=1day&symb=ca:aoi'}
+            ]},
+        {id: 'LUC.TO', text: 'Lucara Diamonds Corp', links: [
+                {text: 'Avanza OMX', link: 'https://www.avanza.se/aktier/om-aktien.html/325050/lucara-diamond-corp'},
+                {text: 'Avanza TMX', link: 'https://www.avanza.se/aktier/om-aktien.html/311297/lucara-diamond-corp'},
+                {text: 'Avanza Forum', link: 'https://www.avanza.se/placera/forum/forum/lucara-diamond-corp.html'},
+                {text: 'Canadian Insider', link: 'https://canadianinsider.com/node/7?menu_tickersearch=luc'},
+                {text: 'Stockwatch Shorts', link: 'http://www.stockwatch.com/Analytics/Shorts.aspx?action=go&symbol=LUC&region=C'},
+                {text: 'Stockhouse', link: 'http://www.stockhouse.com/companies/bullboard/t.luc/lucara-diamond-corp'},
+                {text: 'Big Charts Canada', link: 'http://bigcharts.marketwatch.com/kaavio.Webhost/charts/big.chart?type=64&size=4&state=9&style=380&freq=5mi&time=1day&symb=ca:luc'}
+            ]},
+        {id: 'GIX.TO', text: 'Geologix Explorations Inc.'},
+        {id: 'FOE.OL', text: 'F.OLSEN ENERGY'},
+        {id: 'HM-B.ST', text: 'HM-B'},
+        {id: '^VIX', text: 'VOLATILITY S&P-500'}
+    ]
